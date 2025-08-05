54bb4231d8e28642fd3982bf38414e16eba5d019
nourassili <nourassilicontact@gmail.com>
2025-08-04 22:09:07 -0400

    Fix: add Webpack alias for @

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Add any other config options here
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
