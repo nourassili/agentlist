c8272e36f8f1b7d4d066f1c1b3bf50b9242c1f21
nourassili <nourassilicontact@gmail.com>
2025-08-04 22:48:52 -0400

    Fix casing issue for Footer import

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
         {children}
-        <Footer />
+        <Footer/>
       </body>
     </html>
   );
